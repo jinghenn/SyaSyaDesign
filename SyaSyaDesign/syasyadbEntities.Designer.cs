c056b78068f3f49f6531b51af834a1490e945fa9
mevicca <meviccacheong@gmail.com>
2021-09-12 16:36:28 +0800

    Update entity

--- SyaSyaDesign/syasyadbEntities.Designer.cs
+++ SyaSyaDesign/syasyadbEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\JingHenn\Desktop\SyaSyaDesign\SyaSyaDesign\syasyadbEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Mevicca\source\repos\jinghenn\SyaSyaDesign\SyaSyaDesign\syasyadbEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
